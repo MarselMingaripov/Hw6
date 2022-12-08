a06302db719ddfabf2e81341b45ee271554fdce5
MarselMingaripov <bard1812@gmail.com>
2022-12-08 12:09:38 +0300

    выполнил задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //task1
+        System.out.println();
         for (int i = 1; i <= 10; i++){
             System.out.print(i + " ");
         }
@@ -9,5 +10,14 @@ public class Main {
         for (int i = 10; i >= 1; i--){
             System.out.print(i + " ");
         }
+        System.out.println();
+        //task3
+        for (int i = 0; i <= 17; i++){
+            if (i % 2 == 0 && i != 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
     }
 }
