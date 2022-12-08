85df9ea761fa432c70cd92225ed4b786e4d50ae9
MarselMingaripov <bard1812@gmail.com>
2022-12-08 12:15:26 +0300

    выполнил задание 5

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,9 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.print(i + " год является високосным ");
+        }
+        System.out.println();
     }
 }
