5df1e99d917c279c0b774e48498f4afce66a01a7
MarselMingaripov <bard1812@gmail.com>
2022-12-08 12:20:02 +0300

    выполнил задание 7

--- src/Main.java
+++ src/Main.java
@@ -30,5 +30,8 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
+        for (int i = 1; i <= 512; i *= 2){
+            System.out.print(i + " ");
+        }
     }
 }
