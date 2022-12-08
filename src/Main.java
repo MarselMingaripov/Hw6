cb8ebaf000042159631e41e5b4ef3c59fcc25206
MarselMingaripov <bard1812@gmail.com>
2022-12-08 12:24:35 +0300

    выполнил задание 8

--- src/Main.java
+++ src/Main.java
@@ -17,21 +17,31 @@ public class Main {
                 System.out.print(i + " ");
             }
         }
+        //task4
         System.out.println();
         for (int i = 10; i >= -10; i--){
             System.out.print(i + " ");
         }
+        //task5
         System.out.println();
         for (int i = 1904; i <= 2096; i = i + 4){
             System.out.print(i + " год является високосным ");
         }
+        //task6
         System.out.println();
         for (int i = 7; i <= 98; i = i + 7){
             System.out.print(i + " ");
         }
+        //task7
         System.out.println();
         for (int i = 1; i <= 512; i *= 2){
             System.out.print(i + " ");
         }
+        //task8
+        System.out.println();
+        for (int i = 1; i <= 12; i++){
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + 29000 * i + " рублей");
+        }
+
     }
 }
